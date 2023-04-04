8a50f89b6f2487d5507b35e003caa430f317c791
joaoborges <joaoborgesbiz@gmail.com>
2023-04-04 10:17:46 +0100

    Simple React app Where we create books edit and delete using various hooks

---
+++ src/components/BookList.js
@@ -0,0 +1,21 @@
+import { useContext } from 'react';
+import BooksContext from '../context/books';
+import BookShow from './BookShow';
+
+function BookList() {
+  const { books } = useContext(BooksContext);
+
+  const renderedBooks = books.map((book) => {
+    return (
+     <BookShow key={book.id} book={book} />
+    );
+  });
+
+  return (
+    <div className="book-list">
+      {renderedBooks}
+    </div>
+  );
+};
+
+export default BookList;
---
+++ src/components/BookShow.js
@@ -0,0 +1,45 @@
+import { useState, useContext } from 'react';
+import BooksContext from '../context/books';
+import BookEdit from './BookEdit';
+
+function BookShow({ book }) {
+  const [showEdit, setShowEdit] = useState(false);
+  const { deleteBookById } = useContext(BooksContext);
+
+  const handleDeleteclick = () => {
+    deleteBookById(book.id);
+  };
+
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+
+  const handleSubmit = () => {
+    setShowEdit(false);
+  };
+
+  let content = <h3>{book.title}</h3>;
+  if (showEdit) {
+    content = <BookEdit  onSubmit={handleSubmit} book={book} />;
+  }
+
+  return (
+    <div className="book-show">
+      <img
+        alt="books"
+        src={`https://picsum.photos/seed/${book.id}/300/200`}
+      />
+      <div>{content}</div>
+      <div className="actions">
+        <button className="edit" onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteclick}>
+           Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default BookShow;
